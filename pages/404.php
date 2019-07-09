829f7bfaa0069dcc442737e1707a83c58adeae86
fl3xice <fl3xice@users.noreply.github.com>
2019-07-09 09:39:46 +0300

    404 error update

--- pages/404.php
+++ pages/404.php
@@ -7,11 +7,11 @@
 	<link rel="stylesheet" href="node_modules/animate.css/animate.min.css">
 </head>
 <body class="centered">
-	<div class="error-404 bounceInDown animated">
+	<div class="error-404 ">
 
-		<img src="public/img/logo.png">
+		<img src="public/img/logo.png" class="bounceInDown delay-1s animated">
 
-		<h1>Ухх... простите мы не смогли найти то что вы хотели</h1>
+		<h1 class="bounceInDown animated">Страница не найдена!</h1>
 	</div>
 </body>
 </html>
