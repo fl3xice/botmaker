4aaa52e1b3d3b446abcb01b30d7e772891e90a06
fl3xice <fl3xice@users.noreply.github.com>
2019-07-09 11:28:23 +0300

    Update mainwindow.php

--- pages/mainwindow.php
+++ pages/mainwindow.php
@@ -2,13 +2,27 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>MAIN DOCUMENT</title>
+	<title>BOTMAKER</title>
 	<link rel="stylesheet" href="public/styles/main.css">
 	<link rel="stylesheet" href="node_modules/animate.css/animate.min.css">
+
 </head>
 <body>
 
-	<h1>Hello / Привет</h1>
+	<header>
+		<div class="nav">
+			<ul>
+				<li><a href="#">О Проекте</a></li>
+				<li><a href="#">Войти</a></li>
+				<li><a href="#">Создать аккаунт</a></li>
+			</ul>
+		</div>
+	</header>
+
+
+<script src="http://cdncore.ru/jquery/jquery-3.4.1.min.js"></script>
+
+
 
 </body>
 </html>
