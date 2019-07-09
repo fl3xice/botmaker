22e4a31b86771b06d1d447e6def44ed5887e6b7c
fl3xice <fl3xice@users.noreply.github.com>
2019-07-09 06:56:57 +0300

    Added routing

--- index.php
+++ index.php
@@ -1,5 +1,17 @@
 <?php
+// Require composer autoloader
+require __DIR__ . '/vendor/autoload.php';
 
-$_URI_PAGE = $_SERVER;
+// Create Router instance
+$router = new \Bramus\Router\Router();
 
-print_r($_URI_PAGE);
+// Define routes
+$router->match('GET', 'test', function() {
+	require "pages/test.php";
+});
+$router->match('GET', '/', function() {
+	require "pages/mainwindow.php";
+});
+
+// Run it!
+$router->run();
