27053cb36f5d67eea3248690ff183c7d41627cac
fl3xice <fl3xice@users.noreply.github.com>
2019-07-09 09:17:37 +0300

    Added framework for animations

--- pages/404.php
+++ pages/404.php
@@ -4,9 +4,10 @@
 	<meta charset="UTF-8">
 	<title>Ошибка 404</title>
 	<link rel="stylesheet" href="public/styles/main.css">
+	<link rel="stylesheet" href="node_modules/animate.css/animate.min.css">
 </head>
 <body class="centered">
-	<div class="error-404">
+	<div class="error-404 bounceInDown animated">
 
 		<img src="public/img/logo.png">
 
